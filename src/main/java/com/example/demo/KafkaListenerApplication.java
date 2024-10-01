40e3d6a7235c154c15288ea9438723e9edbe2357
unknown <jahanvi.bansal@gmail.com>
2024-10-01 14:21:19 +0530

    Updated for fixes

--- src/main/java/com/example/demo/KafkaListenerApplication.java
+++ src/main/java/com/example/demo/KafkaListenerApplication.java
@@ -1,14 +1,23 @@
 package com.example.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.core.task.support.TaskExecutorAdapter;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -27,9 +36,33 @@ public class KafkaListenerApplication {
     }
     
     //Used if managing kafka consumers with an executor
+    
+    @Bean
+    public ExecutorService executorService() {
+        return Executors.newVirtualThreadPerTaskExecutor();
+    }
+    
     @Bean
     public AsyncTaskExecutor applicationTaskExecutor() {
-        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+        return new TaskExecutorAdapter(executorService());
     }
-  
+    
+    Map<String, Object> props = new HashMap<>();
+    @Bean
+    public KafkaConsumer<String, String> kafkaConsumer() {
+       
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "my-consumer-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        return new KafkaConsumer<>(props);
+    }
+
+    // You might want to create a ConsumerFactory as well
+    @Bean
+    @DependsOn("kafkaConsumer")
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
 }
